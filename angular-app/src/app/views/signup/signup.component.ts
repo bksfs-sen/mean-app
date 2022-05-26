a6798fedc8af0ca700bd0ea7a49055122809cafe
bharat <bharat@amwebtech.com>
2022-05-26 16:08:16 +0530

    checking issue

--- angular-app/src/app/views/signup/signup.component.ts
+++ angular-app/src/app/views/signup/signup.component.ts
@@ -7,42 +7,65 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  userForm: FormGroup = new FormGroup({
+  userForm: any = new FormGroup({
     firstName: new FormControl('')
   })
   submitted = false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.configUserForm()
+    this.configUserForm()
   }
 
   configUserForm() {
-    // this.userForm = this.fb.group({
-    //   firstName: [
-    //     '',
-    //     [
-    //       Validators.required,
-    //       Validators.minLength(3),
-    //       Validators.maxLength(10),
-    //       Validators.pattern('^[a-zA-Z]+$')
-    //     ]
-    //   ]
-    // })
+    this.userForm = this.fb.group(
+      {
+        firstName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern('^[a-zA-Z]+$'),
+          ],
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3)
+          ]
+        ]
+      }
+    )
   }
-  get f(): { [key: string]: AbstractControl } {
+
+  get invalidError() {
     return this.userForm.controls;
   }
 
   saveForm() {
     this.submitted = true;
-    if (this.userForm.invalid) {
+    console.log("userForm.invalid======", this.userForm.valid)
+    if (this.userForm.valid) {
       console.log("0000", JSON.stringify(this.userForm.value, null, 2));
       return;
     } else {
       console.log("1111", JSON.stringify(this.userForm.value, null, 2));
 
     }
+  }
+
+
+  validateInputKey(formKey: any) {
+    if (this.userForm.get(formKey) && this.userForm.get(formKey).invalid) {
+      return 'text-danger'
+    } else {
+      return 'text-success'
+    }
+  }
+
+  resetForm() {
+    this.configUserForm()
   }
 
 }
